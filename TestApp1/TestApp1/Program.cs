e88e03c71366b6ee80f2063744b6afeb14620a86
rfegsu <fergusmolloy@btinternet.com>
2018-04-26 19:58:28 +0100

    minor updates

--- TestApp1/TestApp1/Program.cs
+++ TestApp1/TestApp1/Program.cs
@@ -2,12 +2,55 @@
 
 namespace TestApp1
 {
+    class general
+    {
+        public static void printInt32(int[] arr)
+        {
+            string str = "";
+            for(int x =0; x<arr.Length; x++)
+            {
+                str = str + arr[x] + ",";
+            }
+            Console.WriteLine(str);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Player hero = new Player(name);
+            Console.WriteLine("Hello {0}", name);
+            Console.WriteLine("Your starting stats are:");
+            general.printInt32(hero.getStats());
             Console.ReadKey();
         }
+    }
+    class Player
+    {
+        private string name;
+        private int health;
+        private int strength;
+        private int dexterity;
+        private int mana;
+
+        public Player(string name)
+        {
+            this.name = name;
+            this.health = 100;
+            this.strength = 20;
+            this.dexterity = 20;
+            this.mana = 20;
+        }
+
+        public int[] getStats()
+        {
+            int[] arr = { health, strength, dexterity, mana };
+            return arr;
+        }
+
+
     }
 }
